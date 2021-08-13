96b9e23727edf95097bcf1c8d187eaad42f25f3b
Gustavo Henrique de Paula Moreira Guimarães <gustavo_gs55@hotmail.com>
2021-08-13 09:11:48 -0300

    Estudo da função reduce

--- Array/map3.js
+++ Array/map3.js
@@ -26,11 +26,3 @@ let values = carrinho.map2(toObject).map2(valueMoney).map2(toMoney);
 
 console.log(values);
 
-const codeBarText ="1234567899123546678911234123455001234567899874"
-
-
-function formateToCodeBar(stringCodeBar){
-    return `${stringCodeBar.substring(0,5)}.${stringCodeBar.substring(5,5)} ${stringCodeBar.substring(10,5)}.${stringCodeBar.substring(15,6)} ${stringCodeBar.substring(21,5)}.${stringCodeBar.substring(26,6)} ${stringCodeBar.substring(32,1)} ${stringCodeBar.substring(33,14)}`;
-}
-
-console.log(formateToCodeBar(codeBarText));
---
+++ Array/reduce1.js
@@ -0,0 +1,23 @@
+const alunos = [
+    {nome: 'João Paulo', nota: 23.6, bolsista: false},
+    {nome: 'Bia Martins', nota: 24.4, bolsista: true},
+    {nome: 'Gabriel Vitor', nota: 27.8, bolsista: true},
+    {nome: 'Maria Paula', nota: 30.5, bolsista: false},
+    {nome: 'Anna Luiza', nota: 27.6, bolsista: false},
+    {nome: 'Heitor Pinto', nota: 30, bolsista: true},
+]
+
+console.log(alunos.map(a => a.nota),"\n");//retorna um novo array só com as notas.
+
+//sem parametro de valor inicial
+const resul1 = alunos.map(a => a.nota).reduce(function(acumulador, valueAtual){
+    console.log(acumulador +" / "+valueAtual);//mostrar o passo a passo 
+    return acumulador + valueAtual;
+});
+
+console.log("--\n")
+
+//com parametro de valor inicial
+const resul2 = alunos.map(a => a.nota).reduce(function(acumulador, valueAtual){
+    return acumulador + valueAtual;
+}, 10);
